990ada691019ed73f2a4a7b000ad8aff0f353234
sofianeradjah <s.radjah.megara@gmail.com>
2022-03-08 16:18:22 +0100

    Correction type variable et fonctions pour respecter normes PSR :) Location.php

--- Model/Location.php
+++ Model/Location.php
@@ -7,7 +7,7 @@ class Location{
     private string $latitude;
     private string $img;
 
-    public function __construct($name, $description = "", $longitude, $latitude, $img = null)
+    public function __construct(string $name, string $longitude, string $latitude, string $description = "", string $img = null)
     {
         $this->name = $name;
         $this->description = $description;
@@ -16,35 +16,45 @@ class Location{
         $this->img = $img;
     }
 
-    public function getName(){
+    public function getName(): string
+    {
         return $this->name;
     }
-    public function getDescription(){
+    public function getDescription(): string
+    {
         return $this->description;
     }
-    public function getLongitude(){
+    public function getLongitude(): string
+    {
         return $this->longitude;
     }
-    public function getLatitude(){
+    public function getLatitude(): string
+    {
         return $this->latitude;
     }
-    public function getImg(){
+    public function getImg(): string
+    {
         return $this->img;
     }
 
-    public function setName($name){
+    public function setName(string $name)
+    {
         $this->name = $name;
     }
-    public function setDescription($description){
+    public function setDescription(string $description)
+    {
         $this->description0 = $description;
     }
-    public function setLongitude($longitude){
+    public function setLongitude(string $longitude)
+    {
         $this->longitude = $longitude;
     }
-    public function setLatitude($latitude){
+    public function setLatitude(string $latitude)
+    {
         $this->latitude = $latitude;
     }
-    public function setImg($img){
+    public function setImg(string $img)
+    {
         $this->img = $img;
     }
 }
