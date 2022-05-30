339ac572628710d94bb5d660543802516779052e
mathieu.cantogrel <mathieu.cantogrel@gmail.com>
2022-05-30 13:34:29 +0200

    update du model User

--- Model/User.php
+++ Model/User.php
@@ -4,31 +4,23 @@ include_once('Model/Connexion.php');
 class User
 {
     private int $id;
-    private string $login;
     private string $mail;
+    private string $username;
     private string $password;
+    private bool $is_admin;
 
-    public function __construct(int $id, string $login, string $mail, string $password)
+    public function __construct(int $id, string $mail, string $username, string $password, bool $is_admin)
     {
         $this->id = $id;
-        $this->login = $login;
         $this->password = $password;
+        $this->username = $username;
         $this->mail = $mail;
+        $this->is_admin = $is_admin;
     }
 
     public function getId(): int
     {
         return $this->id;
-    }
-
-    public function getLogin(): string
-    {
-        return $this->login;
-    }
-
-    public function setLogin(string $login)
-    {
-        return $this->login = $login;
     }
 
     public function getMail(): string
@@ -41,6 +33,16 @@ class User
         return $this->mail = $mail;
     }
 
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username)
+    {
+        return $this->username = $username;
+    }
+
     public function getPassword(): string
     {
         return $this->password;
@@ -51,6 +53,11 @@ class User
         return $this->password = $password;
     }
 
+    public function isAdmin(): bool
+    {
+        return $this->is_admin;
+    }
+
     public static function findById(int $id)
     {
         $pdo = Connexion::connect();
@@ -58,17 +65,27 @@ class User
         $req->execute(['id' => $id]);
 
         $user = $req->fetch();
-        return new User($user['id'], $user['login'], $user['mail'], $user['password']);
+        return new User($user['id'], $user['mail'], $user['username'], $user['password'], $user['is_admin']);
     }
 
-    public static function findByLogin(string $login)
+    public static function findByUsername(string $username)
     {
         $pdo = Connexion::connect();
-        $req = $pdo->prepare('SELECT * FROM user WHERE login = :login');
-        $req->execute(['login' => $login]);
+        $req = $pdo->prepare('SELECT * FROM user WHERE username = :username');
+        $req->execute(['username' => $username]);
 
         $user = $req->fetch();
-        return new User($user['id'], $user['login'], $user['mail'], $user['password']);
+        return new User($user['id'], $user['mail'], $user['username'], $user['password'], $user['is_admin']);
+    }
+
+    public static function findByMail(string $mail)
+    {
+        $pdo = Connexion::connect();
+        $req = $pdo->prepare('SELECT * FROM user WHERE mail = :mail');
+        $req->execute(['mail' => $mail]);
+
+        $user = $req->fetch();
+        return new User($user['id'], $user['mail'], $user['username'], $user['password'], $user['is_admin']);
     }
 
     public static function findAll() {
@@ -79,29 +96,40 @@ class User
         $users = [];
         while ($user = $req->fetch())
         {
-            $users[] = new User($user['id'], $user['login'], $user['mail'], $user['password']);
+            $users[] = new User($user['id'], $user['mail'], $user['username'], $user['password'], $user['is_admin']);
         }
         return $users;
     }
 
-    public static function create(string $login, string $mail, string $password)
+    public static function create(string $mail, string $username, string $password)
     {
         $pdo = Connexion::connect();
-        $req = $pdo->prepare('INSERT INTO user (login, mail, password) VALUES (:login, :mail, :password)');
+        $req = $pdo->prepare('INSERT INTO user (mail, username, password) VALUES (:mail, :username, :password)');
         $req->execute([
-            'login' => $login,
             'mail' => $mail,
+            'username' => $username,
             'password' => $password
         ]);
     }
 
-    public static function update(int $id, string $login, string $mail, string $password)
+    public static function createAdmin(string $mail, string $username, string $password)
     {
         $pdo = Connexion::connect();
-        $req = $pdo->prepare('UPDATE user SET login = :login, mail = :mail, password = :password WHERE id = :id');
+        $req = $pdo->prepare('INSERT INTO user (mail, username, password, is_admin) VALUES (:mail, :username, :password, 1)');
         $req->execute([
-            'login' => $login,
             'mail' => $mail,
+            'username' => $username,
+            'password' => $password
+        ]);
+    }
+
+    public static function update(int $id, string $mail, string $username, string $password)
+    {
+        $pdo = Connexion::connect();
+        $req = $pdo->prepare('UPDATE user SET mail = :mail, username = :username, password = :password WHERE id = :id');
+        $req->execute([
+            'mail' => $mail,
+            'username' => $username,
             'password' => $password,
             'id' => $id
         ]);
