092414d0c3d177586fd22a0b77cfc06ebc7b7ca5
Sofiane RADJAH <55430903+Sofrad83@users.noreply.github.com>
2022-06-02 16:16:02 +0200

    Listing course bdd ok + pagination ok

--- Model/Course.php
+++ Model/Course.php
@@ -4,13 +4,15 @@ class Course
 {
     private int $id;
     private string $name;
+    private ?string $url_img;
     private DateTime $created_at;
     private DateTime $updated_at;
 
-    public function __construct(int $id, string $name, DateTime $created_at, DateTime $updated_at)
+    public function __construct(int $id, string $name, ?string $url_img, DateTime $created_at, DateTime $updated_at)
     {
         $this->id = $id;
         $this->name = $name;
+        $this->url_img = $url_img;
         $this->created_at = $created_at;
         $this->updated_at = $updated_at;
     }
@@ -36,6 +38,17 @@ class Course
         $this->updated_at = new DateTime();
     }
 
+    public function getUrlImg(): ?string
+    {
+        return $this->url_img;
+    }
+
+    public function setUrlImg(?string $url_img)
+    {
+        $this->url_img = $url_img;
+        $this->updated_at = new DateTime();
+    }
+
     public function getCreatedAt(): DateTime
     {
         return $this->created_at;
@@ -53,41 +66,50 @@ class Course
         $req->execute(['id' => $id]);
 
         $course = $req->fetch();
-        return new course($course['id'], $course['name'], $course['created_at'], $course['updated_at']);
+        return new course($course['id'], $course['name'], $course['url_img'], $course['created_at'], $course['updated_at']);
     }
 
-    public static function findAll() {
+    public static function findAll(int $page, int $nb): array
+    {
         $pdo = Connexion::connect();
-        $req = $pdo->prepare('SELECT * FROM course');
-        $req->execute();
 
+        $req = $pdo->prepare('SELECT * FROM course LIMIT :deb, :nb');
+        $deb = $nb*($page-1);
+        $req->bindParam("deb", $deb, PDO::PARAM_INT);
+        $req->bindParam("nb", $nb, PDO::PARAM_INT);
+        $req->execute();
         $courses = [];
         while ($course = $req->fetch())
         {
-            $courses[] = new course($course['id'], $course['name'], $course['created_at'], $course['updated_at']);
+            $created_at = new DateTime($course['created_at']);
+            $updated_at = new DateTime($course['updated_at']);
+            $courses[] = new Course($course['id'], $course['name'], $course['url_img'], $created_at, $updated_at);
         }
         return $courses;
     }
 
-    public static function create(int $id, string $name, string $created_at, string $updated_at)
+    public static function create(string $name, ?string $url_img)
     {
+        $date = new DateTime();
         $pdo = Connexion::connect();
-        $req = $pdo->prepare('INSERT INTO course (name, created_at, updated_at) VALUES (:name, :created_at, :updated_at)');
+        $req = $pdo->prepare('INSERT INTO course (name, url_img, created_at, updated_at) VALUES (:name, :url_img, :created_at, :updated_at)');
         $req->execute([
             'name' => $name,
-            'created_at' => new DateTime(),
-            'updated_at' => new DateTime()
+            'url_img' => $url_img,
+            'created_at' => $date->format("Y-m-d H:i:s"),
+            'updated_at' => $date->format("Y-m-d H:i:s")
         ]);
     }
 
-    public static function update(int $id, string $name, string $created_at, string $updated_at)
+    public static function update(int $id, string $name, string $url_img, string $created_at, string $updated_at)
     {
+        $date = new DateTime();
         $pdo = Connexion::connect();
-        $req = $pdo->prepare('UPDATE course SET name = :name, created_at = :created_at, updated_at = :updated_at');
+        $req = $pdo->prepare('UPDATE course SET name = :name, url_img = :url_img, created_at = :created_at, updated_at = :updated_at');
         $req->execute([
             'name' => $name,
-            'created_at' => new DateTime(),
-            'updated_at' => new DateTime(),
+            'url_img' => $url_img,
+            'updated_at' => $date->format("Y-m-d H:i:s"),
             'id' => $id
         ]);
     }
@@ -97,5 +119,14 @@ class Course
         $pdo = Connexion::connect();
         $req = $pdo->prepare('DELETE FROM course WHERE id = :id');
         $req->execute(['id' => $id]);
+    }
+
+    public static function count()
+    {
+        $pdo = Connexion::connect();
+        $req = $pdo->prepare('SELECT count(*) as count FROM course');
+        $req->execute();
+        $result = $req->fetch();
+        return $result['count'];
     }
 }
