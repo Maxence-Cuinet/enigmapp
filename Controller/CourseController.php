5c57be677779299f1b79b3cdfd51b09d93740765
mathieu.cantogrel <mathieu.cantogrel@gmail.com>
2022-06-05 23:10:00 +0200

    ptites corrections

--- Controller/CourseController.php
+++ Controller/CourseController.php
@@ -10,22 +10,19 @@ class CourseController
 
     public static function deleteCourse()
     {
-        $id = isset($_REQUEST['id']) ? $_REQUEST['id'] : "";
-        if(empty($id)){
+        if (!AuthController::isLogged(true) || !isset($_REQUEST['id'])) {
+            header("Location: /");
+        }
+
+        try {
+            Course::delete((int) $_REQUEST['id']);
             echo json_encode([
-                'message' => "Erreur : impossible de trouver le jeu de piste."
-            ], JSON_UNESCAPED_UNICODE); //-> Pour éviter des erreurs d'encodage avec les accents
-        }else{
-            try{
-                Course::delete((int)$id);
-                echo json_encode([
-                    'message' => "Succès : le jeu de piste a été supprimé"
-                ], JSON_UNESCAPED_UNICODE);
-            }catch(PDOException $e){
-                echo json_encode([
-                    'message' => "Erreur :".$e->getMessage()
-                ], JSON_UNESCAPED_UNICODE);
-            }
+                'message' => "Succès : le jeu de piste a été supprimé"
+            ], JSON_UNESCAPED_UNICODE);
+        } catch (PDOException $e) {
+            echo json_encode([
+                'message' => "Erreur :" . $e->getMessage()
+            ], JSON_UNESCAPED_UNICODE);
         }
         die;
     }
--- view/homeView.php
+++ view/homeView.php
@@ -12,7 +12,7 @@
                 <a type="button" class="btn btn-outline-dark ms-3" href="/add-course"><i class="fa-solid fa-plus me-2"></i><b>Ajouter</b></a>
             <?php } ?>
         </div>
-        <?php if(empty($_POST['courses'])) {?>
+        <?php if (empty($_POST['courses'])) {?>
             <div class="row mt-5">
                 <div class="col text-center">
                     <h1>Aucun jeu de piste enregistré. <i class="fa fa-face-sad-cry text-warning"></i></h1>
@@ -22,7 +22,7 @@
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4">
                 <?php 
                 $courses = $_POST['courses'];
-                foreach($courses as $course) {
+                foreach ($courses as $course) {
                 ?>
                     <div class="col mb-5">
                         <div class="card h-100">
@@ -39,56 +39,57 @@
                                             <a class="btn btn-outline-dark mt-auto" href="#">Participer</a>
                                         </div>
                                     </div>
-                                    <div class="row">
-                                        <div class="col p-0 d-flex justify-content-end">
-                                            <a href="javascript:void(0)">
-                                            <i class="fa fa-edit text-primary fa-xl"></i>
-                                            </a>
-                                            &nbsp;
-                                            <a href="javascript:void(0)" data-name="<?php echo $course->getName() ?>" data-id="<?php echo $course->getId() ?>" class="delete-course">
-                                                <i class="fa fa-trash text-danger fa-xl"></i>
-                                            </a>
+                                    <?php if(AuthController::isLogged(true)) {?>
+                                        <div class="row">
+                                            <div class="col p-0 d-flex justify-content-end">
+                                                <a href="javascript:void(0)">
+                                                <i class="fa fa-edit text-primary fa-xl"></i>
+                                                </a>
+                                                &nbsp;
+                                                <a href="javascript:void(0)" data-name="<?php echo $course->getName() ?>" data-id="<?php echo $course->getId() ?>" class="delete-course">
+                                                    <i class="fa fa-trash text-danger fa-xl"></i>
+                                                </a>
+                                            </div>
                                         </div>
-                                    </div>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
                     </div>
                 <?php } ?>
             </div>
-            <br>
-            <div class="row">
+            <div class="row mt-4">
                 <div class="col d-flex justify-content-center">
                     <nav aria-label="Page navigation example">
                         <ul class="pagination">
                             <li class="page-item">
-                            <a class="page-link" href="<?php echo $_POST['page'] == 1 ? 'javascript:void(0)' : "/home?page=".($_POST['page']-1) ?>" aria-label="Previous">
+                            <a class="page-link" href="<?php echo $_POST['page'] === 1 ? 'javascript:void(0)' : "/home?page=" . ($_POST['page'] - 1) ?>" aria-label="Previous">
                                 <span aria-hidden="true">&laquo;</span>
                             </a>
                             </li>
 
                             <?php if (in_array($_POST['page'], [1, 2])) { ?>
-                                <li class="page-item <?php echo $_POST['page'] == 1 ? 'active' : ''?>"><a class="page-link" href="/home?page=1">1</a></li>
-                                <li class="page-item <?php echo $_POST['page'] == 2 ? 'active' : ''?>"><a class="page-link" href="/home?page=2">2</a></li>
+                                <li class="page-item <?php echo $_POST['page'] === 1 ? 'active' : ''?>"><a class="page-link" href="/home?page=1">1</a></li>
+                                <li class="page-item <?php echo $_POST['page'] === 2 ? 'active' : ''?>"><a class="page-link" href="/home?page=2">2</a></li>
                                 <li class="page-item"><a class="page-link" href="/home?page=3">3</a></li>
                                 <li class="page-item"><a class="page-link" href="/home?page=4">4</a></li>
                                 <li class="page-item"><a class="page-link" href="/home?page=5">5</a></li>
-                            <?php } else if (in_array($_POST['page'], [$_POST['max'], $_POST['max']-1])) { ?>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['max']-4) ?>"><?php echo $_POST['max']-4 ?></a></li>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['max']-3) ?>"><?php echo $_POST['max']-3 ?></a></li>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['max']-2) ?>"><?php echo $_POST['max']-2 ?></a></li>
-                                <li class="page-item <?php echo $_POST['page'] == $_POST['max']-1 ? 'active' : ''?>"><a class="page-link" href="<?php echo "/home?page=".($_POST['max']-1) ?>"><?php echo $_POST['max']-1 ?></a></li>
-                                <li class="page-item <?php echo $_POST['page'] == $_POST['max'] ? 'active' : ''?>"><a class="page-link" href="<?php echo "/home?page=".$_POST['max'] ?>"><?php echo $_POST['max'] ?></a></li>
+                            <?php } else if (in_array($_POST['page'], [$_POST['max'], $_POST['max'] - 1])) { ?>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['max'] - 4) ?>"><?php echo $_POST['max'] - 4 ?></a></li>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['max'] - 3) ?>"><?php echo $_POST['max'] - 3 ?></a></li>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['max'] - 2) ?>"><?php echo $_POST['max'] - 2 ?></a></li>
+                                <li class="page-item <?php echo $_POST['page'] === $_POST['max'] - 1 ? 'active' : ''?>"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['max'] - 1) ?>"><?php echo $_POST['max'] - 1 ?></a></li>
+                                <li class="page-item <?php echo $_POST['page'] === $_POST['max'] ? 'active' : ''?>"><a class="page-link" href="<?php echo "/home?page=" . $_POST['max'] ?>"><?php echo $_POST['max'] ?></a></li>
                             <?php } else { ?>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['page']-2) ?>"><?php echo $_POST['page']-2 ?></a></li>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['page']-1) ?>"><?php echo $_POST['page']-1 ?></a></li>
-                                <li class="page-item active"><a class="page-link" href="<?php echo "/home?page=".($_POST['page']) ?>"><?php echo $_POST['page'] ?></a></li>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['page']+1) ?>"><?php echo $_POST['page']+1 ?></a></li>
-                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=".($_POST['page']+2) ?>"><?php echo $_POST['page']+2 ?></a></li>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['page'] - 2) ?>"><?php echo $_POST['page'] - 2 ?></a></li>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['page'] - 1) ?>"><?php echo $_POST['page'] - 1 ?></a></li>
+                                <li class="page-item active"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['page']) ?>"><?php echo $_POST['page'] ?></a></li>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['page'] + 1) ?>"><?php echo $_POST['page'] + 1 ?></a></li>
+                                <li class="page-item"><a class="page-link" href="<?php echo "/home?page=" . ($_POST['page'] + 2) ?>"><?php echo $_POST['page'] + 2 ?></a></li>
                                 
                             <?php } ?>
                             <li class="page-item">
-                            <a class="page-link" href="<?php echo $_POST['page'] == $_POST['max'] ? 'javascript:void(0)' : "/home?page=".($_POST['page']+1) ?>" aria-label="Next">
+                            <a class="page-link" href="<?php echo $_POST['page'] === $_POST['max'] ? 'javascript:void(0)' : "/home?page=" . ($_POST['page'] + 1) ?>" aria-label="Next">
                                 <span aria-hidden="true">&raquo;</span>
                             </a>
                             </li>
